28c431ed3d46439b3cf3f07560a5bdb6854d358e
Bobby Lei <bobl53@gmail.com>
2016-05-10 13:50:44 +1200

    adding the user model

--- web/models/user.ex
+++ web/models/user.ex
@@ -6,12 +6,13 @@ defmodule PhoenixTrello.User do
     field :last_name, :string
     field :email, :string
     field :encrypted_password, :string
+    field :password, :string, virtual: true
 
     timestamps
   end
 
-  @required_fields ~w(first_name last_name email encrypted_password)
-  @optional_fields ~w()
+  @required_fields ~w(first_name last_name email password)
+  @optional_fields ~w(encrypted_password)
 
   @doc """
   Creates a changeset based on the `model` and `params`.
@@ -22,5 +23,19 @@ defmodule PhoenixTrello.User do
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:password, min: 5)
+    |> validate_confirmation(:password, message: "Password does not match")
+    |> unique_constraint(:email, message: "Email already taken")
+    |> generate_encrypted_password
+  end
+
+  defp generate_encrypted_password(current_changeset) do
+    case current_changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: password}} ->
+        put_change(current_changeset, :encrypted_password, Comeonin.Bcrypt.hashpwsalt(password))
+      _ ->
+      current_changeset
+    end
   end
 end
