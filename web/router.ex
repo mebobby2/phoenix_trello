bdd80eed9539b8871f7e63abaf679b598b753d2c
Bobby Lei <bobl53@gmail.com>
2016-05-10 14:13:07 +1200

    adding route for registrations

--- web/router.ex
+++ web/router.ex
@@ -20,7 +20,11 @@ defmodule PhoenixTrello.Router do
   end
 
   # Other scopes may use custom stacks.
-  # scope "/api", PhoenixTrello do
-  #   pipe_through :api
-  # end
+  scope "/api", PhoenixTrello do
+    pipe_through :api
+
+    scope "/v1" do
+      post "/registrations", RegistrationController, :create
+    end
+  end
 end
