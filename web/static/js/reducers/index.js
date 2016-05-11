7652a4ab6018ee69c630d0df1760f15e5c5402cb
Bobby Lei <bobl53@gmail.com>
2016-05-12 10:37:58 +1200

    boards front-end code

--- web/static/js/reducers/index.js
+++ web/static/js/reducers/index.js
@@ -2,9 +2,13 @@ import { combineReducers }  from 'redux';
 import { routeReducer }     from 'react-router-redux';
 import session              from './session';
 import registration         from './registration';
+import boards               from './boards';
+import currentBoard         from './current_board';
 
 export default combineReducers({
   routing: routeReducer,
   session: session,
   registration: registration,
+  boards: boards,
+  currentBoard: currentBoard,
 });
